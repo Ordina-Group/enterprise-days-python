cd670ec0a8123d03e233cadf4270a9dcb2175b6a
Johan Vergeer <johanvergeer@gmail.com>
2022-03-18 13:20:25 +0100

    save sku in the database so it can be used in queries

--- recipe_store/recipes/models.py
+++ recipe_store/recipes/models.py
@@ -1,23 +1,55 @@
+from abc import abstractmethod
+from typing import Any
+
 from django.db import models
 
 
-class Recipe(models.Model):
+class SkuModelMixin(models.Model):
+    sku = models.CharField(max_length=11, primary_key=True)
+
+    class Meta:
+        abstract = True
+
+    @property
+    @abstractmethod
+    def _sku_prefix(self) -> str:
+        """unique 3 letter prefix for the sku"""
+        ...
+
+    def _set_sku(self) -> None:
+        if self.sku == "":
+            latest_recipe = self.__class__.objects.order_by("sku").last()
+            if latest_recipe is not None:
+                self.sku = f"{self._sku_prefix}{(int(latest_recipe.sku[3:]) + 1):08d}"
+            else:
+                self.sku = f"{self._sku_prefix}{1:08d}"
+
+
+class Recipe(SkuModelMixin):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
     ingredients = models.ManyToManyField("Ingredient", through="RecipeIngredient")
 
     @property
-    def sku(self) -> str:
-        return f"PIZ{self.id:08d}"
+    def _sku_prefix(self) -> str:
+        return "PIZ"
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        self._set_sku()
+        super(Recipe, self).save(*args, **kwargs)
 
 
-class Ingredient(models.Model):
+class Ingredient(SkuModelMixin):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
 
     @property
-    def sku(self) -> str:
-        return f"ING{self.id:08d}"
+    def _sku_prefix(self) -> str:
+        return "ING"
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        self._set_sku()
+        super(Ingredient, self).save(*args, **kwargs)
 
 
 class RecipeIngredient(models.Model):
--- recipe_store/recipes/tests.py
+++ recipe_store/recipes/tests.py
@@ -85,15 +85,38 @@ class TestRecipeModel(TestCase):
         self.assertEqual(pizza.ingredients.count(), 2)
 
     def test_sku(self) -> None:
+        # Test whether the initial sku is created
         recipe = Recipe.objects.create(
             name="Schnitzel pizza", description="For the true schnitzel lover!"
         )
-
         self.assertEqual(recipe.sku, "PIZ00000001")
+
+        # Test whether the second sku is created
+        recipe_2 = Recipe.objects.create(
+            name="Another pizza", description="Just another pizza!"
+        )
+        self.assertEqual(recipe_2.sku, "PIZ00000002")
+
+        # Test whether the third sku is created, and the ordering of skus is correct
+        recipe_3 = Recipe.objects.create(
+            name="Yet another pizza", description="Just another pizza!"
+        )
+        self.assertEqual(recipe_3.sku, "PIZ00000003")
 
 
 class TestIngredientModel(TestCase):
     def test_sku(self) -> None:
+        # Test whether the initial sku is created
         ingredient = Ingredient.objects.create(name="Dough", description="The dough")
 
         self.assertEqual(ingredient.sku, "ING00000001")
+
+        # Test whether the second sku is created
+        ingredient_2 = Ingredient.objects.create(name="Sauce", description="The Sauce")
+        self.assertEqual(ingredient_2.sku, "ING00000002")
+
+        # Test whether the third sku is created, and the ordering of skus is correct
+        ingredient_3 = Ingredient.objects.create(
+            name="Cheeeeze", description="Whichever flavor you want"
+        )
+        self.assertEqual(ingredient_3.sku, "ING00000003")
