e0b29f3d64d078d4f9f034c9ca059bed144abeba
Johan Vergeer <johanvergeer@gmail.com>
2022-03-20 10:53:02 +0100

    get Django SECRET_KEY from environment variable

--- recipe_store/recipe_store/settings.py
+++ recipe_store/recipe_store/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wk2@9k-q_mw2jtpv*cs7prusmx6ej-6(gj!1a##_bhe+6)3p$8"
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
